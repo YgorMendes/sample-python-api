253ae16948db50ea6b38b2d4c4c0278127e1b1b4
Ygor Mendes <ygortmendes@gmail.com>
2024-12-05 21:26:30 -0300

    fix: return conection with supabase

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
 from models import MovieModel, UserModel
 import os
-# from supabase import create_client
+from supabase import create_client
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 
 # Conexão com dataBase
-# url: str = os.environ.get("SUPABASE_URL")
-# key: str = os.environ.get("SUPABASE_KEY")
-# client = create_client(url, key)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+client = create_client(url, key)
 
 @app.route('/createMovie', methods=['POST'])
 def create_movie():
@@ -18,7 +18,7 @@ def create_movie():
       movie_data = MovieModel(**request.json)
       movie_dict = movie_data.model_dump()
       
-      # client.table("movies").insert(movie_dict).execute()
+      client.table("movies").insert(movie_dict).execute()
 
       return '', 201
   except Exception as e:
@@ -30,7 +30,7 @@ def create_user():
       user_data = UserModel(**request.json)
       user_dict = user_data.model_dump()
 
-      # client.table('users').insert(user_dict).execute();
+      client.table('users').insert(user_dict).execute()
 
       return '', 201
   except Exception as e:
@@ -39,9 +39,9 @@ def create_user():
 @app.route('/movies/<gender>', methods=['GET'])
 def find_movies_by_gender(gender: str):
   try:
-    # response = client.table("movies").select('*').filter('gender', 'cs', f'{{"{gender}"}}').execute()
+    response = client.table("movies").select('*').filter('gender', 'cs', f'{{"{gender}"}}').execute()
 
-    return "Bombou", 200
+    return jsonify({"data": response.data}), 200
   except Exception as e:
     return jsonify({"error": str(e)}), 400
 
